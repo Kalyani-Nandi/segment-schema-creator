acb046e1db467a65008dfd08ed095bf7d5198013
Kalyani Nandi <kalyani.nandi@nettantra.net>
2024-09-25 23:10:21 +0530

    react project added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SegmentCreationModal from './components/SegmentCreationModal';
+
+function App() {
+  return (
+    <div>
+ <SegmentCreationModal/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SegmentCreationModal.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const SegmentCreationModal = () => {
+  return <div className="">SegmentCreationModal</div>;
+};
+export default SegmentCreationModal;
