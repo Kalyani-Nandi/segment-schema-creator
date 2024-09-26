cee16a1b8744a2be355c616c67c4b5c33f61930f
Kalyani Nandi <kalyani.nandi@nettantra.net>
2024-09-26 13:13:47 +0530

    added schema creation

--- src/components/SegmentCreationModal.js
+++ src/components/SegmentCreationModal.js
@@ -1,12 +1,42 @@
 import React, { useState } from "react";
 
+const options = [
+  { label: "First Name", value: "first_name" },
+  { label: "Last Name", value: "last_name" },
+  { label: "Gender", value: "gender" },
+  { label: "Age", value: "age" },
+  { label: "Account Name", value: "account_name" },
+  { label: "city", value: "city" },
+  { label: "state", value: "state" },
+];
 const SegmentCreationModal = ({ handleCloseModal }) => {
   const [segmentName, setSegmentName] = useState("");
- 
+  const [selectedSchema, setSelectedSchema] = useState("");
 
-  const saveSagment = (e) => {
-    e.preventDefault();
-    console.log(segmentName);
+  const [schemas, setSchemas] = useState([]);
+  const [schemaOptions, setSchemaOptions] = useState(options);
+
+  const saveSagment = () => {
+   const schema= schemas.map((schema) => ({
+      [schema]: options.find((option) => option.value == schema)?.label,
+    }))
+    const data = {
+      segment_name: segmentName,
+      schema: schema,
+    };
+
+    console.log(data);
+  };
+
+  const addSchema = () => {
+    if (!selectedSchema) return;
+    if (selectedSchema) {
+      setSchemas([...schemas, selectedSchema]);
+      setSchemaOptions((prevOptions) =>
+        prevOptions.filter((option) => option.value !== selectedSchema)
+      );
+      setSelectedSchema("");
+    }
   };
 
   return (
@@ -32,7 +62,7 @@ const SegmentCreationModal = ({ handleCloseModal }) => {
         </div>
 
         <div className="p-10">
-          <div className="flex flex-col gap-5 text-base">
+          <div className="flex flex-col gap-5 text-base mb-4">
             <label className="font-bold text-gray-700 ">
               Enter the Name of the Segment
             </label>
@@ -44,10 +74,47 @@ const SegmentCreationModal = ({ handleCloseModal }) => {
               onChange={(e) => setSegmentName(e.target.value)}
             />
           </div>
+
+          <div className="flex flex-col gap-5 text-base">
+            <select
+              className="border-2 border-gray-600 py-1.5 px-2.5 w-[70%] rounded"
+              onChange={(e) => setSelectedSchema(e.target.value)}
+            >
+              <option value="">{"Add Schema to segment"}</option>
+              {schemaOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+
+            <button
+              onClick={() => addSchema()}
+              className="py-1.5 text-teal-600 text-base font-semibold rounded-md underline decoration-teal-600 flex justify-start"
+            >
+              + Add new Schema
+            </button>
+          </div>
+          <div className="flex flex-col gap-5 text-base mt-4">
+            {schemas.map((schema, i) => {
+              return (
+                <div
+                  className="border-2 border-gray-600 py-1.5 px-2.5 w-[70%] rounded"
+                  key={i}
+                >
+                  <span>
+                    {options.find((option) => option.value === schema)?.label ||
+                      schema}
+                  </span>
+                </div>
+              );
+            })}
+          </div>
         </div>
+
         <div className="bg-gray-200 px-10 gap-10 py-5 font-bold text-base flex justify-between items-center">
           <button
-            onClick={(e) => saveSagment(e)}
+            onClick={() => saveSagment()}
             className="px-10 py-1.5 bg-teal-500 rounded-md text-white"
           >
             Save the Segment
